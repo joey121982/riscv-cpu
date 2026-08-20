2551deef95bd22bcf05a22f5d960f8a50ec224a7
joey121982 <bratescuandrei17@gmail.com>
2026-08-20 20:44:38 +0300

    added initial ISS code setup

---
+++ iss/README.md
@@ -0,0 +1,6 @@
+# RV32I Instruction Set Simulator
+This directory contains the source code for the ISS used as a "golden model" for simulation.
+
+## Usage
+
+The included Makefile builds both a CLI tool to directly run RV32I code, as well as a ```.so``` object to be linked into a UVM testbench. 
---
+++ iss/src/include/dpi_wrapper.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void*       iss_create();
+void        iss_destroy(void* iss_ptr);
+void        iss_reset(void* iss_ptr);
+int32_t     iss_load_program(void* iss_ptr, const char* filepath);
+
+int32_t     iss_step(void* iss_ptr);
+uint32_t    iss_get_pc(void* iss_ptr);
+uint32_t    iss_get_reg(void* iss_ptr, int reg_index);
+uint32_t    iss_read_mem(void* iss_ptr, uint32_t addr);
+
+uint32_t    iss_get_last_inst(void* iss_ptr);
+const char* iss_get_disassembly(void* iss_ptr);
+
+#ifdef __cplusplus
+}
+#endif
