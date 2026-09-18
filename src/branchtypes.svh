5c0ea416c7b100c4f05b62820f446f1940c84563
joey121982 <bratescuandrei17@gmail.com>
2026-09-18 23:36:43 +0300

    branchtypes.svh bug fix

--- src/branchtypes.svh
+++ src/branchtypes.svh
@@ -2,12 +2,12 @@
 `define BRANCHTYPES_SVH
 
 typedef enum logic [2:0] {
-    BR_EQ,
-    BR_NEQ,
-    BR_LT,
-    BR_GE,
-    BR_LTU,
-    BR_GEU
+    BR_EQ  = 3'b000,
+    BR_NEQ = 3'b001,
+    BR_LT  = 3'b100,
+    BR_GE  = 3'b101,
+    BR_LTU = 3'b110,
+    BR_GEU = 3'b111
 } br_t;
 
 `endif // BRANCHTYPES_SVH
--- src/mmio.sv
+++ src/mmio.sv
@@ -10,9 +10,9 @@ module mmio #(
     input   logic       [31:0]  w_data,
     input   mem_size_t          mem_size,
 
-    input   logic       [0:NUM_PORTS-1][31:0]   pin_in      , // IDR
-    output  logic       [0:NUM_PORTS-1][31:0]   pin_out     , // ODR
-    output  logic       [0:NUM_PORTS-1][31:0]   pin_dir     , // DDR
+    input   logic       [0:NUM_PORTS-1][31:0]   pin_in,     // IDR
+    output  logic       [0:NUM_PORTS-1][31:0]   pin_out,    // ODR
+    output  logic       [0:NUM_PORTS-1][31:0]   pin_dir,    // DDR
     output  logic       [31:0]  r_data
 );
 
