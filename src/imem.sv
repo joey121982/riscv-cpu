a37f8a66fc08910352c41705eb917b23510b1b44
joey121982 <bratescuandrei17@gmail.com>
2026-09-18 21:50:54 +0300

    added program memory zeroing

--- src/imem.sv
+++ src/imem.sv
@@ -7,7 +7,13 @@ module imem #(
 
 logic [31:0] memory [0:8191];
 
-initial $readmemh(INIT_FILE, memory);
+initial begin 
+    integer i;
+    for (i = 0; i < 8192; i = i + 1) begin
+        memory[i] <= 32'b0;
+    end
+    $readmemh(INIT_FILE, memory);
+end
 
 assign inst = memory[addr[14:2]];   // divide the address by 4 by truncating
 
